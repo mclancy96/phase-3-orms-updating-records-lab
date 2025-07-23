50c1aa70378435fcc2be6dee59ad50aa78ee40a1
Mike <mclancy96@gmail.com>
2025-07-23 09:12:11 -0400

    Initialize and create

--- lib/student.rb
+++ lib/student.rb
@@ -1,9 +1,23 @@
-require_relative "../config/environment.rb"
+require_relative '../config/environment'
 
 class Student
+  attr_accessor :name, :grade, :id
 
-  # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]
+  def initialize(name, grade, id = nil)
+    @id = id
+    @name = name
+    @grade = grade
+  end
 
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS students (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        grade TEXT
+      )
+    SQL
 
+    DB[:conn].execute(sql)
+  end
 end
